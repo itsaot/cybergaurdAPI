046de343a41cd9ec98636f9a66c4254f97226912
itsaot <calvinmzoboshe@gmail.com>
2025-10-02 20:27:33 +0200

    ,,

--- routes/reports.js
+++ routes/reports.js
@@ -23,7 +23,7 @@ router.get('/', getAllReports);               // View all reports
 
 router.get('/flagged', isAdmin, getFlaggedReports);   // View flagged only
 router.get('/:id', auth, isAdmin, getReportById);           // View single report by ID
-router.patch('/:id/flag', isAdmin, flagReport);       // Flag a report
+router.patch('/:id/flag'auth, isAdmin, flagReport);       // Flag a report
 router.delete('/:id', auth, isAdmin, deleteReport);         // Delete a report
 router.patch('/:id/react', auth, reactToReport);            // React to a report
 
